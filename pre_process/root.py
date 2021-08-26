d7788ee0b22830c9ad9a0a7c4f67f8870757f214
Greg Atkins <greg@ingenii.dev>
2021-08-26 18:39:04 +0100

    Find function

--- pre_process/root.py
+++ pre_process/root.py
@@ -1,9 +1,18 @@
-# Example configuration:
-# from functions import json_to_csv
-# pre_process_functions = {
-#     "data_provider": {
-#         "table": json_to_csv
-#     }
-# }
+import typing
 
-pre_process_functions = {}
+
+# Example configuration:
+# from functions import json_to_csv, other_function
+# def find_pre_process_function(data_provider: str, table_name: str
+#                               ) -> Union[None, FunctionType]:
+#     if data_provider == "provider1" and table_name == "table1":
+#         return json_to_csv
+#     elif data_provider == "provider2":
+#         return other_function
+
+from typing import Union
+from types import FunctionType
+
+def find_pre_process_function(data_provider: str, table_name: str
+                              ) -> Union[None, FunctionType]:
+    return
